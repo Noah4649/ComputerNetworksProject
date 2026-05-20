0936dcf4993a2139dc1948ba9ded24b10c508162
Benjamin Holliday <Ben.H321@outlook.com>
2026-05-20 19:27:29 +0800

    working on functions under Host class

--- devices.py
+++ devices.py
@@ -2,12 +2,20 @@ from config import *
 from protocol import frame, packet, segment
 
 class Host:
-    def __init__(self, name, ip, mac):
+    def __init__(self, name, ip, mac, default_gateway_ip):
         self.name = name
         self.ip = ip
         self.mac = mac
         self.mac_table = {} # {ip: mac}
+        self.default_gateway_ip = default_gateway_ip
+        self.next_seq = 0
     
+    #Creates a new segment with alternating sequence number
+    def create_data_segment(self, src_port, dst_port, data):
+        seg = segment(src_port, dst_port, data, "DATA", self.next_seq)
+        self.next_seq = 1 - self.next_seq
+        return seg
+
     #MAC learning
     def learn_mac(self, ip, mac):
         self.mac_table[ip] = mac
@@ -17,24 +25,51 @@ class Host:
     def lookup_mac(self, ip):
         return self.mac_table.get(ip, None)
     
+    #Calculate next IP hop for destination
+    def next_hop(self, dst_ip):
+        if dst_ip.rsplit(".", 1)[0] == self.ip.rsplit(".", 1)[0]:
+            return dst_ip
+        else:
+            return self.default_gateway_ip
+    
     #Send frame
     def send_frame(self, dst_mac, packet):
         f = frame(self.mac, dst_mac, packet)
+        print(f"{self.name}: Layer 2: Packet recieved from Network Layer")
         print(f"{self.name}: Layer 2: Frame created: SRC_MAC={self.mac}, DST_MAC={dst_mac}")
+        print(f"{self.name}: Layer 2: Frame sent")
         return f
     
     #Recieve frame
     def receive_frame(self, f):
-        self.learn_mac(f.data.src_ip, f.src_mac)
+        self.learn_mac(f.packet.src_ip, f.src_mac)
         print(f"{self.name}: Layer 2: Frame received")
         print(f"{self.name}: Layer 2: Packet delivered to Network Layer")
-        return f.data  
+        return f.packet
     
     def receive_packet(self, pkt):
         print(f"{self.name}: Layer 3: Packet received from Data Link Layer: SRC_IP={pkt.src_ip}, DST_IP={pkt.dst_ip}, TTL={pkt.ttl}")
         print(f"{self.name}: Layer 3: Destination IP read: {pkt.dst_ip}")
-        print(f"{self.name}: Layer 3: Packetindentified as local delivery.")
-        return pkt.data  
+        print(f"{self.name}: Layer 3: Packet indentified as local delivery")
+        print(f"{self.name}: Layer 3: Segment delivered to Transport Layer")
+
+        seg = pkt.segment
+
+        print(f"{self.name}: Layer 4: Segment recieved from Network Layer")
+
+        if not seg.verify_checksum():
+            print(f"{self.name}: Layer 4: Segment discarded due to checksum error")
+        else: 
+            print(f"{self.name}: Layer 4: Checksum verfied")
+
+            if seg.type == "DATA":
+                print(f"{self.name}: Layer 4: Data delivered to Application Layer")
+                return self.send_ack(pkt) #### TO DO
+            
+            elif seg.type == "ACK":
+                print(f"{self.name}: Layer 4: ACK recieved: seq={seg.seq_num}")
+        
+        return None
     
 class Router:
     def __init__(self, name, interfaces, routing_table):
